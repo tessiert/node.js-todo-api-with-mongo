70e5f8aae99e27075847d5a5ecc7438c8a25681e
Trace Tessier <tessiert@unm.edu>
2018-12-03 14:50:31 -0700

    Added Get /todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,13 @@ app.post('/todos/', (request, response) => {
     });
 });
 
+app.get('/todos/', (request, response) => {
+    Todo.find().then((todos) => {
+        response.send({todos});
+    }, (error) => {
+        response.status(400).send(error);
+    });
+});
 
 
 app.listen(3000, () => {
