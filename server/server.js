8afed116782c06d2ac201fd909bfcf2e82fab91e
Trace Tessier <tessiert@unm.edu>
2018-12-03 11:19:51 -0700

    Created Todo POST route and refactored server.js

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos/', (request, response) => {
+    var todo = new Todo({
+        text: request.body.text
+    });
+
+    todo.save().then((doc) => {
+        response.send(doc);
+    }, (error) => {
+        response.status(400).send(error);
+    });
+});
+
+
+
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
+
+
